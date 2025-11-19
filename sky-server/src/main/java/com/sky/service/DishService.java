f6f78124e50de791f936bfc36dec9c0bf7e90eb9
sherrylxf <1298512407@qq.com>
2025-11-19 18:51:42 +0800

    day2.3

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,4 +10,5 @@ public interface DishService {
 
     void saveWithFlavor(DishDTO dishDTO);
 
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
