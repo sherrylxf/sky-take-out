ec9cf55897c6dc2b580c482944505deba02e9ada
sherrylxf <1298512407@qq.com>
2025-11-21 16:38:56 +0800

    day4.2

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "C端-店铺相关接口")
+@Slf4j
+public class ShopController {
+    public static final String SHOP_STATUS = "SHOP_STATUS";// 店铺营业状态
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺营业状态
+     * @param status
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取营业状态")
+    public Result<Integer> getStatus(@PathVariable Integer status) {
+        Integer shopstatus = (Integer) redisTemplate.opsForValue().get(SHOP_STATUS);
+        log.info("获取营业状态: {}", shopstatus == 1 ? "营业中" : "打烊中");
+        return Result.success(shopstatus);
+    }
+}
